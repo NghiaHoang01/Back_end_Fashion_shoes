9865b63106d9c126ff6b8e0f1c39493eb91b8572
NghiaHoang01 <bebebechecheche@gmail.com>
2024-01-23 17:54:19 +0700

    update "vnp_CreateDate" of VNPay

--- src/main/java/com/example/service/implement/OrderServiceImpl.java
+++ src/main/java/com/example/service/implement/OrderServiceImpl.java
@@ -163,7 +163,7 @@ public class OrderServiceImpl implements OrderService {
         String vnp_CreateDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
 
-        cld.add(Calendar.MINUTE, 15);
+        cld.add(Calendar.MINUTE, 2880);
         String vnp_ExpireDate = formatter.format(cld.getTime());
         vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
 

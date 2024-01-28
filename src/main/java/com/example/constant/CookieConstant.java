dbbd0e0adb147533fb958baa0a268afb61229973
NghiaHoang01 <bebebechecheche@gmail.com>
2024-01-28 15:13:10 +0700

    Separate tokens into admin and user in cookie

--- src/main/java/com/example/constant/CookieConstant.java
+++ src/main/java/com/example/constant/CookieConstant.java
@@ -3,9 +3,13 @@ package com.example.constant;
 public class CookieConstant {
     public static final String OTP_COOKIE = "otp_cookie";
 
-    public static final String JWT_COOKIE ="token_cookie";
+    public static final String JWT_COOKIE_USER ="token_cookie_user";
 
-    public static final String JWT_REFRESH_CODE_COOKIE ="token_refresh_code_cookie";
+    public static final String JWT_COOKIE_ADMIN ="token_cookie_admin";
+
+    public static final String JWT_REFRESH_TOKEN_CODE_COOKIE_USER ="token_refresh_code_cookie_user";
+
+    public static final String JWT_REFRESH_TOKEN_CODE_COOKIE_ADMIN ="token_refresh_code_cookie_admin";
 
     public static final String EMAIL_COOKIE ="email_cookie";
 }

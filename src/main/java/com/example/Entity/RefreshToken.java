70746b67b712dbe6fe427b545e6ddc1c23bd9f7c
NghiaHoang01 <bebebechecheche@gmail.com>
2024-01-28 15:14:40 +0700

    change relationship between table user and table refresh token

--- src/main/java/com/example/Entity/RefreshToken.java
+++ src/main/java/com/example/Entity/RefreshToken.java
@@ -12,8 +12,8 @@ public class RefreshToken {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
     @Column(nullable = false, unique = true)

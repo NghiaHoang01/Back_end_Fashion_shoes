0cbf11ecbe9f513a6dbb5758eb79aa53f4eac94e
NghiaHoang01 <bebebechecheche@gmail.com>
2024-01-28 15:05:09 +0700

    no message

--- src/main/java/com/example/config/AppConfig.java
+++ src/main/java/com/example/config/AppConfig.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,7 +25,7 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-public class AppConfig {
+public class AppConfig{
     @Autowired
     private JwtValidator jwtValidator;
 
